e67f97f43eb17db9848bc9773e2f5c0c81e10bc7
Yiyoung Liu <yiyoungl@andrew.cmu.edu>
2026-07-12 14:54:06 -0400

    frontend: scope deno to frontend for deno-check
    Signed-off-by: Yiyoung Liu <yiyoungl@andrew.cmu.edu>

--- devenv.nix
+++ devenv.nix
@@ -35,6 +35,9 @@
   cachix.enable = false;
 
   git-hooks.excludes = [ "old-frontend/.*" ];
+  git-hooks.hooks.deno-check.entry = lib.mkForce ''
+    bash -c 'cd frontend && args=(); for f in "$@"; do args+=("''${f#frontend/}"); done; deno check "''${args[@]}"' --
+  '';
   git-hooks.hooks.deno-test.entry = lib.mkForce "deno test --ignore=.devenv,.direnv,old-frontend --permit-no-files";
 
   treefmt.config.settings.excludes = [ "old-frontend/**" ];
