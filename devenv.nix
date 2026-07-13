0e29e43dde88c47d99c31fd7518e9101c41c5d51
Anish Pallati <i@anish.land>
2026-07-13 12:50:58 -0400

    nix: modernize devenv patterns
    Signed-off-by: Anish Pallati <i@anish.land>

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,4 @@
 {
-  config,
-  pkgs,
   lib,
   inputs,
   ...
@@ -19,34 +17,30 @@
     deno.enable = true;
     postgres.enable = true;
 
-    ricochet.enable = true;
-    ricochet.appUrl = "http://localhost:8080";
-
-    kennel.services.tartan-vote = {
-      customDomain = "api.tartan.vote";
+    ricochet = {
+      enable = true;
+      appUrl = "http://localhost:8080";
     };
-
-    kennel.sites.frontend = {
-      spa = true;
-      customDomain = "tartan.vote";
+    kennel = {
+      sites.frontend = {
+        spa = true;
+        customDomain = "tartan.vote";
+      };
+      services.tartan-vote.customDomain = "api.tartan.vote";
     };
   };
 
-  cachix.enable = false;
+  git-hooks.hooks = {
+    deno-check.entry = lib.mkForce ''
+      bash -c 'cd frontend && args=(); for f in "$@"; do args+=("''${f#frontend/}"); done; deno check "''${args[@]}"' --
+    '';
+    deno-test.entry = lib.mkForce "deno test --ignore=.devenv,.direnv,old-frontend --permit-no-files";
+  };
 
   git-hooks.excludes = [ "old-frontend/.*" ];
-  git-hooks.hooks.deno-check.entry = lib.mkForce ''
-    bash -c 'cd frontend && args=(); for f in "$@"; do args+=("''${f#frontend/}"); done; deno check "''${args[@]}"' --
-  '';
-  git-hooks.hooks.deno-test.entry = lib.mkForce "deno test --ignore=.devenv,.direnv,old-frontend --permit-no-files";
-
   treefmt.config.settings.excludes = [ "old-frontend/**" ];
 
   scripts = {
     generate-api.exec = "cd frontend && deno task generate-api";
-  };
-
-  env = {
-    VAULT_ADDR = "https://secrets2.scottylabs.org";
   };
 }
